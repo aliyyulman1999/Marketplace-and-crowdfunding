6895e11d7a292307dbe0c0752f92ca64fe25b5d2
Aliyyulman Jihan <49016762+aliyyulman1999@users.noreply.github.com>
2020-11-24 18:02:54 +0700

    "Menambahkan Fitur Marketplace dan databasenya"

---
+++ app/src/main/java/org/d3if4070/minakitaapp/activities/HomeActivity.java
@@ -0,0 +1,14 @@
+package org.d3if4070.minakitaapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import org.d3if4070.minakitaapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/org/d3if4070/minakitaapp/adapter/PostAdapter.java
@@ -0,0 +1,95 @@
+package org.d3if4070.minakitaapp.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import org.d3if4070.minakitaapp.R;
+import org.d3if4070.minakitaapp.activities.PostDetailActivity;
+import org.d3if4070.minakitaapp.model.Post;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.MyViewHolder> {
+
+    Context mContext;
+    List<Post> mData ;
+
+
+    public PostAdapter(Context mContext, List<Post> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View row = LayoutInflater.from(mContext).inflate(R.layout.row_post_item,parent,false);
+        return new MyViewHolder(row);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.tvTitle.setText(mData.get(position).getTitle());
+        holder.tvDana.setText(mData.get(position).getDana());
+        Glide.with(mContext).load(mData.get(position).getPicture()).into(holder.imgPost);
+        Glide.with(mContext).load(mData.get(position).getUserPhoto()).into(holder.imgPostProfile);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle, tvDana;
+        ImageView imgPost;
+        ImageView imgPostProfile;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            tvTitle = itemView.findViewById(R.id.row_post_title);
+            tvDana = itemView.findViewById(R.id.row_post_dana);
+            imgPost = itemView.findViewById(R.id.row_post_img);
+            imgPostProfile = itemView.findViewById(R.id.row_post_profile_img);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent postDetailActivity = new Intent(mContext, PostDetailActivity.class);
+                    int position = getAdapterPosition();
+
+                    postDetailActivity.putExtra("title",mData.get(position).getTitle());
+                    postDetailActivity.putExtra("postImage",mData.get(position).getPicture());
+                    postDetailActivity.putExtra("description",mData.get(position).getDescription());
+                    postDetailActivity.putExtra("dana",mData.get(position).getDana());
+                    postDetailActivity.putExtra("postKey",mData.get(position).getPostKey());
+                    postDetailActivity.putExtra("userPhoto",mData.get(position).getUserPhoto());
+                    // will fix this later i forgot to add user name to post object
+                    //postDetailActivity.putExtra("userName",mData.get(position).getUsername);
+                    long timestamp  = (long) mData.get(position).getTimeStamp();
+                    postDetailActivity.putExtra("postDate",timestamp) ;
+                    mContext.startActivity(postDetailActivity);
+
+
+                }
+            });
+
+        }
+
+
+    }
+}
+
